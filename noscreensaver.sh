8f1905b0e25e56f5eec8bc441f0f3ec4dcd60d2e
AngeZanetti <ange.zanetti@gmail.com>
2015-11-28 15:48:44 +0100

    add the screensaver handling for awesome

---
+++ noscreensaver.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+sleep 1 && xset -dpms && echo 'DPMS off'
+sleep 1 && xset s off && echo 'Screensaver off'
+
+echo 'Caffeine Injected'
---
+++ withscreensaver.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sleep 1 && xset +dpms && echo 'DMPS on'
+sleep 1 && xset s on && echo 'Screensaver on'
+
+echo 'Caffeine Crash Imminent...'
+
